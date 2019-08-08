901e53fdb8ce02e2cf5ddbb1ae45bddf8c5af6d2
meganecummings <megcummings@gmail.com>
2019-08-08 14:14:35 -0700

    Urls Init

--- Beautify/urls.py
+++ Beautify/urls.py
@@ -2,4 +2,8 @@ from django.urls import path
 from . import views
 
 path('status', views.json_res, name='status' )
-path('', views.items, name='item_list')
+path('', views.home_view, name='home_view')
+path('looks/', views.looks, name='looks_list')
+path('items/', views.looks, name='items_list')
+path('shopping_bag/', views.shopping_bag, name='shopping_bag')
+path('checkout/', views.checkout, name='checkout')
