47fc133b67032c8ff2eb22ed02364f5656f26934
maekellachung <maekellaxo@gmail.com>
2019-08-12 12:36:51 -0700

    adds add_to_cart function - still needs to fix

--- Beautify/views.py
+++ Beautify/views.py
@@ -36,10 +36,36 @@ def about(request):
   return render(request, 'about.html')
 
 # Purchasing
+  def add_to_cart(request, item_id):
+    user = request.user
+    #if there is an open order attached to user
+    if Order.objects.filter(user=user).exists():
+      order = Order.objects.get(user=user)
+    # create order_item with order
+      order_item = OrderItem.objects.create(item=item_id, order=order)
+        order_item.save()
+        # redirect to order_view
+        return redirect('order_view', item=item_id)
+    # else create order open
+    else:
+      # create order attached to user
+      user_order = Order.objects.create(user=user, quantity=0)
+      user_order.save()
+      order_item = OrderItem.objects.create(order=order, item=item_id)
+      # create order_item with new order2
+        item = Item.objects.get(pk=item_id)
+        item.quantity += 1
+        order.save()
+        redirect to order_view
+        messages.success(request, f'You have successfully added { item.name } to your cart!')
+
+
 def order_view(request):
   user = request.user
-  orders = Order.objects.filter(user=user)
-  return render(request, 'order_view.html', {'orders': orders, 'user': user})
+  # item = Item.objects.get(id=pk)
+  # order_item = Order.objects.get(id=order_item)
+  order = Order.objects.filter(user=user)
+  return render(request, 'order_view.html', {'user': user, 'order': order})
 
 # def checkout(request):
 
