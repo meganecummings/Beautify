726f90ae31959de0525b573922334759f11e5d44
meganecummings <megcummings@gmail.com>
2019-08-12 21:56:51 -0700

    Removed Look Item

--- Beautify/admin.py
+++ Beautify/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from .models import Item, Look, Order, LookItem, OrderItem
+from .models import Item, Look, Order, OrderItem
 # Register your models here.
 admin.site.register(Item)
 admin.site.register(Look)
 admin.site.register(Order)
-admin.site.register(LookItem)
 admin.site.register(OrderItem)
