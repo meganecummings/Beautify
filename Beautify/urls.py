d734d1aa30f7679c5012911fd879fa003240abdd
maekellachung <maekellaxo@gmail.com>
2019-08-12 17:20:41 -0700

    adding to cart WORKS! + updated navbar to base

--- Beautify/urls.py
+++ Beautify/urls.py
@@ -10,6 +10,7 @@ path('looks/<int:pk>', views.view_look, name='view_look'),
 path('items/', views.items_list, name='items_list'),
 path('items/<int:pk>', views.view_item, name='view_item'),
 path('order_view/', views.order_view, name='order_view'), 
+path('items/add/<int:pk>', views.add_to_cart, name='add_to_cart'), 
 path('about/', views.about, name='about'),
 path('profile/', views.profile, name='profile')
 # path('checkout/', views.checkout, name='checkout')
