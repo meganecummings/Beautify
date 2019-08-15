f90021ed3123db5b71e9badd92b0046504edb0c3
meganecummings <megcummings@gmail.com>
2019-08-14 19:29:35 -0700

    Got Order View math functionality back

--- Beautify/views.py
+++ Beautify/views.py
@@ -4,8 +4,7 @@ from .models import *
 from django.contrib.auth.decorators import login_required
 import decimal
 import stripe
-stripe.api_key = "sk_test_aKXivJtOPosLqtBi0GzFepuE00nUWZJb61"
-
+stripe.api_key = "pk_test_ZNASggyuMPivZNtUeAVSRigy00Ksb2rkKa"
 
 # Home
 def home(request):
@@ -70,22 +69,18 @@ def add_to_cart(request, pk):
 @login_required
 def order_view(request):
   user = request.user
-
-  # item = Item.objects.get(id=pk)
-  # order_item = Order.objects.get(id=order_item)
-  orders = Order.objects.filter(user=user.pk)
-  # user_order = OrderItem.objects.filter(user=user)
-  return render(request, 'order_view.html', {'orders': orders})
+  # orders = Order.objects.filter(user=user.pk)
+  # return render(request, 'order_view.html', {'orders': orders})
   orders = Order.objects.filter(user=user.pk).filter(purchased=False)
   for order in orders:
     all_items = order.items.all()
     total_price = sum(item.item.price for item in all_items)
-    sales_tax = decimal.Decimal('0.0725')
-    print(sales_tax)
-    estimated_tax = (total_price*sales_tax)
-    estimated_total = (total_price+estimated_tax)
-    print(estimated_total)
-    stripe_total = (estimated_total*100)
+  sales_tax = decimal.Decimal('0.0725')
+  print(sales_tax)
+  estimated_tax = (total_price*sales_tax)
+  estimated_total = (total_price+estimated_tax)
+  print(estimated_total)
+  stripe_total = (estimated_total*100)
 
   return render(request, 'order_view.html', {'orders': orders, 'total_price': total_price, 'estimated_total': estimated_total, 'estimated_tax': estimated_tax, 'stripe_total': stripe_total })
 
